07ff717ec1c9ece59eda0646d49a12637cbbe00b
Niyonkuru-olivier <oniyonkuru44@gmail.com>
2026-07-10 10:37:50 +0200

    Remove debug code from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({
     origin: '*',
@@ -16,10 +14,6 @@ async function bootstrap() {
   const port = process.env.PORT || 5000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
-  console.log('Routes registered:');
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
-  console.log(router);
 }
 
 bootstrap();
